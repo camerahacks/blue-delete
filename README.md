5caff7aed6d9147519cab2360a22a2466a3a7cdb
André Costa <andre@dphacks.com>
2024-11-18 21:17:42 -0600

    Better docs

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ If this script was useful to you and you would like to support my work, you can 
 
 **Please proceed with caution.** There is no going back after you delete your Tweets. Do it at your own risk!
 
+## Step by Step Instructions
+
+Here's how I used my Raspberry Pi to delete all my Tweets. You don't need a Raspberry Pi, though. This is just a Python script.
+
+This script does not require access to the Twitter official API to delete your tweets. Instead, it communicates with Twitter the same way that browser app communicates with TWitter servers.
+
 ## Download your Twitter/X Archive
 
 The first thing you should do is request your Twitter/X archive. Blue Delete uses the archive files to know which Tweets to delete. It usually takes 24 hours for Twitter/X to get your archive ready.
@@ -16,17 +22,9 @@ So, request your archive and work on the other steps below while you wait for yo
 
 You can so by going to ```Settings and privacy > Your account > Download and archive of your data``` either through the X app or the website.
 
-## Step by Step Instructions
+Once the archive is ready for download, unzip the tweet archive and copy the ```tweets.js``` file to the same folder where you placed this python script. ```tweets.js``` contains the information to all your tweets.
 
-Here's how I used my Raspberry Pi to delete all my Tweets.
-
-This script does not require access to the Twitter official API to delete your tweets. Instead, it communicates with Twitter the same way that browser app communicates with TWitter servers.
-
-### Tweet Archive
-
-Unzip the tweet archive and copy the ```tweets.js``` file to the same folder where you placed this python script. ```tweets.js``` contains the information to all your tweets.
-
-### Browser Session Information
+## Browser Session Information
 
 Login to your Twitter/X account through a web browser on a desktop computer. The instructions below are for Chromium but they should be the same in all browsers.
 
@@ -35,8 +33,21 @@ and the network operations it is performing.
 
 Still on your Twitter/X page, go to your profile page and choose a single Tweet to delete. Make sure that network traffic is being recorded. The ```record``` button should be red.
 
-![Record button in developer tools](./media/Record%20Network%20Traffic.png "Record Network Traffic")
+<img src="./media/Record Network Traffic.png" alt="Record button in developer tools" width="600"/>
 
 Delete the Tweet and press the red ```record``` button to stop recording network traffic.
 
-Find the entry for 
+Find the entry labeled ```DeleteTweet```. Right-click on this entry and choose ```Copy > Copy as cURL (bash)```. This will put the cURL command in your clipboard.
+
+<img src="./media/Record Network Traffic - DeleteTweet.png" alt="Delete Tweet Action in developer tools" width="600"/>
+
+Open the file names ```curl.txt``` and replace all the content of the file with the cURL command from the step above. This will be used to extract all your session and cookie information.
+
+Browser reverse engineering is fun, huh?
+
+## Run The script
+
+Time to run the script and delete all your Tweets. **Proceed with caution, there is no going back.**
+
+The script will read all your tweets from the archive, and will make API calls to delete each one of them. It keeps track of tweets that have been deleted in a file called ```tracking.json```.
+If the process is interrupted for some reason, it doesn't have to start from the beginning again.
--- delete-tweet.py
+++ delete-tweet.py
@@ -19,9 +19,8 @@ def save_tracking(tracking_info):
     with open('tracking.json', 'w') as tracking_file:
          json.dump(tracking_info, tracking_file)
 
-
-
 headers = {}
+tracking = {}
 
 endpoint = "https://twitter.com/i/api/graphql/VaenaVgh5q5ih7kvyVjgtg/DeleteTweet"
 
@@ -29,108 +28,130 @@ endpoint = "https://twitter.com/i/api/graphql/VaenaVgh5q5ih7kvyVjgtg/DeleteTweet
 # Meat and Potatoes
 #####
 
-# Get the header values from the example curl command
-with open('curl.txt') as curlfile:
-    
-    while line := curlfile.readline():
+def main():
+    global tracking
+    # Get the header values from the example curl command
+    with open('curl.txt') as curlfile:
         
-        if("-H" in line):
+        while line := curlfile.readline():
             
-            matches = re.findall(r"'(.*?)'", line)
+            if("-H" in line):
+                
+                matches = re.findall(r"'(.*?)'", line)
 
-            for match in matches:
+                for match in matches:
 
-                splitter = match.find(":")
+                    splitter = match.find(":")
 
-                key = match[:splitter]
-                value = match[splitter+1:].strip()
+                    key = match[:splitter]
+                    value = match[splitter+1:].strip()
 
-                # Add the key and value to the header dict to be used with the request
-                headers[key]=value
+                    # Add the key and value to the header dict to be used with the request
+                    headers[key]=value
 
-# Open the tweet archive file
-with open('tweets.js', encoding='utf8') as tweetfile:
+    # Open the tweet archive file
+    with open('tweets.js', encoding='utf8') as tweetfile:
+            
+            # Find the position of the first opening [
+            # Then replace the opening character with an empty string
+            file_content = tweetfile.read()
+            json_start = file_content.find("[")
+            tweet_archive = file_content[json_start:]
+            archive_json = json.loads(tweet_archive)
+
+            # print(archive_json)
+
+    # Open the tracking file if one exists
+    if(os.path.exists('tracking.json')):
         
-        # Find the position of the first opening [
-        # Then replace the opening character with an empty string
-        file_content = tweetfile.read()
-        json_start = file_content.find("[")
-        tweet_archive = file_content[json_start:]
-        archive_json = json.loads(tweet_archive)
-
-        # print(archive_json)
-
-# Open the tracking file if one exists
-if(os.path.exists('tracking.json')):
-    
-    with open('tracking.json') as tracking_file:
-    
-        tracking = json.load(tracking_file)
-
-else:
-    
-    # Create a tracking dict 
-    tracking = {}
-
-    for tweets in archive_json:
-        tracking[tweets['tweet']['id']] = 'pending'
-
-    # Save tracking file
-    save_tracking(tracking)
-
-pending_deletes = dict(filter(filter_pending, tracking.items()))
-
-# Delete the tweets
-current_count = 0
-count_fail = 0
-count = 0
-
-for key, value in pending_deletes.items():
-
-    tweetId = key
-
-    body = {"variables":{"tweet_id":tweetId,"dark_request":False},"queryId":"VaenaVgh5q5ih7kvyVjgtg"}
-
-    response = requests.post(url=endpoint, headers=headers, json=body)
-
-    count = count + 1
-
-    if(response.status_code == 200):
-
-        tracking[key] = 'deleted'
-
-        current_count = current_count + 1
-
-        # Wait anywhere from 1 second to 3 seconds between requests
-        time.sleep(random.randint(1, 3))
-    
-    elif(response.status_code == 429):
-
-        # Too many attempts, wait 15 minutes
-        time.sleep(901)
+        with open('tracking.json') as tracking_file:
+        
+            tracking = json.load(tracking_file)
 
     else:
 
-        count_fail = count_fail + 1
+        # Create a tracking dict 
+        for tweets in archive_json:
+            tracking[tweets['tweet']['id']] = 'pending'
 
-        if(count_fail > 5):
+        # Save tracking file
+        save_tracking(tracking)
 
-            print('Too many failed attemps. Aborting')
+    pending_deletes = dict(filter(filter_pending, tracking.items()))
 
-            sys.exit()
+    delete_count = len(pending_deletes.keys())
+
+    print("You have "+str(delete_count)+" Tweets to delete.")
+
+    time_to_delete = round(delete_count/100*15/60, 2)
+
+    print("This will take approximately "+str(time_to_delete)+" hours.")
+
+
+    # Delete the tweets
+    current_count = 0
+    count_fail = 0
+    count = 0
+
+    for key, value in pending_deletes.items():
+
+        tweetId = key
+
+        body = {"variables":{"tweet_id":tweetId,"dark_request":False},"queryId":"VaenaVgh5q5ih7kvyVjgtg"}
+
+        response = requests.post(url=endpoint, headers=headers, json=body)
+
+        count = count + 1
+
+        if(response.status_code == 200):
+
+            tracking[key] = 'deleted'
+
+            print("Tweet "+key+" deleted!")
+
+            current_count = current_count + 1
+
+            # Wait anywhere from 1 second to 2 seconds between requests
+            time.sleep(random.randint(1, 2))
+        
+        elif(response.status_code == 429):
+
+            # Too many attempts, wait 15 minutes
+            print("Too many attempts. Waiting 15 minutes...")
+            time.sleep(901)
+
+        else:
+
+            count_fail = count_fail + 1
+
+            if(count_fail > 5):
+
+                print('Too many failed attempts. Aborting. Progress saved.')
+
+                save_tracking(tracking)
+
+                sys.exit()
+        
+        # Wait 5 minutes after 101 requests
+        if(count > 100):
+
+            count = 0
+
+            print("Taking a break. Deleting Tweets will resume shortly")
+            print(str(current_count)+" Tweets deleted. "+str(delete_count-current_count)+" left to go...")
+
+            time.sleep(300)
+
+# Let's GOOOOOO!
+try:
+    if input("Are you sure you want to delete all your Tweets? There is no going back. [y/n]") != "y":
+        sys.exit()
     
-    # Wait 5 minutes after 50 requests
-    if(count > 50):
+    main()
 
-        count = 0
-
-        time.sleep(300)
-
-            
-
-
-
-         
-
-# print(response.status_code)
-# print(response.json)
+except KeyboardInterrupt:
+    
+    save_tracking(tracking)
+    
+    print("Script Interrupted. Progress saved.")
+    sys.exit()
